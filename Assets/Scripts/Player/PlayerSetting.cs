3f5755a1194d88f673c961932aa115de84344490
AripyKSU <121083269+AripyKSU@users.noreply.github.com>
2023-06-28 15:30:27 +0900

    UdonSharp Prefab Resource 버그
    UdonSharp Prefab Resource 버그

---
+++ Assets/Scripts/Player/PlayerSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDKBase;
+using static VRC.SDKBase.Networking;
+using static VRC.SDKBase.VRCPlayerApi;
+using VRC.Udon;
+using UdonSharp;
+
+public class PlayerSetting : UdonSharpBehaviour
+{
+    public GameObject prefab;
+    void Start()
+    {
+        prefab = Resources.Load<GameObject>("Prefabs/Player/PlayerStat");
+        Debug.Log(0);
+        //Debug.Log(1);
+        //playerStat.SetActive(true);
+        //SetOwner(LocalPlayer, playerStat);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
