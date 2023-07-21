974885a1e1607ebe3cf0ccd76888b71081282690
GMH <89373426+GoM1nH@users.noreply.github.com>
2023-07-22 00:34:47 +0900

    UI & Interior add
    - Bookshelf, Chair, Desk, Lamp Add
    - UI Design Upgrade

---
+++ Assets/Resources/GMH/UdonSharp/TestBtn.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TestBtn : UdonSharpBehaviour
+{
+    public override void Interact()
+    {
+        Debug.Log("Btn Active");
+    }
+}
