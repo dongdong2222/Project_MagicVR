3d24a0e765c5907bd212308ddc48fc5ca10ce5eb
GMH <rhalsgur3151@gmail.com>
2023-09-09 18:17:40 +0900

    Model add
    - Magic Wand Add (Model, Texture)
    - Carriage Add (Model, Texture, Animation) => 만약 사용 원할시 고민혁에게 물어보기

---
+++ Assets/Resources/GMH/UdonSharp/WheelTest.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class WheelTest : UdonSharpBehaviour
+{
+    [SerializeField] private Animator[] wheels;
+
+    private bool test = false;
+    public override void Interact()
+    {
+        if (!Networking.IsOwner(gameObject)) { Networking.SetOwner(Networking.LocalPlayer, gameObject); }
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(WheelPlay));
+    }
+
+    public void WheelPlay()
+    {
+        Debug.Log("zz");
+        test = !test;
+
+        for (int i=0; i <wheels.Length; i++)
+        {
+            wheels[i].SetBool("Go", test);
+        }
+    }
+}
